077c356a1fa26f65a6fe941a51d1ca754c7c8267
vladilen597 <vladilen597@gmail.com>
2025-12-24 13:26:47 +0300

    [gr-66] fix: add button

--- app/components/Shared/GoogleLogin/GoogleLogin.tsx
+++ app/components/Shared/GoogleLogin/GoogleLogin.tsx
@@ -1,15 +1,15 @@
 import { googleAuthUrl } from "@/app/lib/utils/googleAuth";
 import { Button } from "../../ui/button";
-
-const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+import Image from "next/image";
 
 export default function GoogleLogin() {
   return (
     <Button
-      className="mt-2"
+      className="mt-2 flex items-center"
       variant="outline"
       onClick={() => (window.location.href = googleAuthUrl())}
     >
+      <Image width={12} height={12} src="/icons/google.svg" alt="Google icon" />
       Continue with Google
     </Button>
   );
