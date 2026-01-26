6ac2c0183dabaa5ca6cad96e6773d0175c1b1a0f
Mihazar <azarenkov.jobs@gmail.com>
2026-01-26 16:16:04 +0300

    [gr-175] Реализован туман войны

--- app/lib/utils/instance.ts
+++ app/lib/utils/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1/back/api";
+const baseURL = "https://dev.myfinishline.io/back/api";
 const paddleBaseUrl = "https://api.paddle.com/v2";
 
 const instance = axios.create({
