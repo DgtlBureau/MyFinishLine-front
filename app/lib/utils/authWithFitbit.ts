be5c2e6cb6c4da0231ac4b347d377c0fd13b1776
Mihazar <azarenkov.jobs@gmail.com>
2026-02-04 23:27:49 +0300

    Test commit

--- app/lib/utils/authWithFitbit.ts
+++ app/lib/utils/authWithFitbit.ts
@@ -1,6 +1,6 @@
 export const getFitbitAuthUrl = () => {
   const clientId = process.env.NEXT_PUBLIC_FITBIT_CLIENT_ID;
-  const redirectUri = "https://dev.myfinishline.io/auth/fitbit/callback";
+  const redirectUri = "https://myfinishline.io/auth/fitbit/callback";
   const state = encodeURIComponent(window.location.pathname);
   return `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=activity%20heartrate%20sleep%20profile&state=${state}`;
 };
